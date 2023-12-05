5c562d63c6d9fe50f3403a99f042e23aaf11138b
SaveliiYam <savelkayamil5@gmail.com>
2023-12-05 19:08:39 +0300

    new file:   cmd/migrator/main.go 	modified:   go.mod 	modified:   go.sum 	modified:   internal/services/auth/auth.go 	new file:   migrations/1_init.down.sql 	new file:   migrations/1_init.up.sql 	new file:   migrations/2_add_is_admin_column_to_users_tbl.down.sql 	new file:   migrations/2_add_is_admin_column_to_users_tbl.up.sql

--- go.mod
+++ go.mod
@@ -4,7 +4,13 @@ go 1.21.4
 
 require github.com/ilyakaznacheev/cleanenv v1.5.0
 
-require github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.16.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -36,6 +36,8 @@ type AppProvider interface {
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidAppID       = errors.New("invalid app ID")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 // New returns a new instance of the Auth service
@@ -100,6 +102,10 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email, password string) (int
 
 	id, err := a.userSaver.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists", sl.Err(err))
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
 		log.Error("failed to save suer", sl.Err(err))
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
@@ -116,6 +122,10 @@ func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
 
 	isAdmin, err := a.userProvider.IsAdmin(ctx, userID)
 	if err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			log.Warn("user not found", sl.Err(err))
+			return false, fmt.Errorf("%s: %w", op, ErrInvalidAppID)
+		}
 		return false, fmt.Errorf("%s: %w", op, err)
 	}
 	log.Info("checked if user is admin", slog.Bool("is_admin", isAdmin))
