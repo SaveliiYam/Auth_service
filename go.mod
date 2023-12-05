893a7a59835bf1ecd0178981e9ccb0d3a7c1d77e
SaveliiYam <savelkayamil5@gmail.com>
2023-12-05 17:23:47 +0300

    modified:   go.mod 	modified:   go.sum 	new file:   internal/lib/jwt/jwt.go 	modified:   internal/services/auth/auth.go 	new file:   internal/storage/storage.go

--- go.mod
+++ go.mod
@@ -4,13 +4,16 @@ go 1.21.4
 
 require github.com/ilyakaznacheev/cleanenv v1.5.0
 
+require github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.16.0
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,10 +2,16 @@ package auth
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
 	"time"
 
 	"github.com/SaveliiYam/ProtoAPI_Auth/internal/domain/models"
+	"github.com/SaveliiYam/ProtoAPI_Auth/internal/lib/jwt"
+	"github.com/SaveliiYam/ProtoAPI_Auth/internal/lib/logger/sl"
+	"github.com/SaveliiYam/ProtoAPI_Auth/internal/storage"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Auth struct {
@@ -28,6 +34,10 @@ type AppProvider interface {
 	App(ctx context.Context, appId int) (models.App, error)
 }
 
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+)
+
 // New returns a new instance of the Auth service
 func New(log *slog.Logger, userSaver UserSaver, userProvider UserProvider, appProvider AppProvider, tokenTTL time.Duration) *Auth {
 	return &Auth{
@@ -37,4 +47,77 @@ func New(log *slog.Logger, userSaver UserSaver, userProvider UserProvider, appPr
 		log:          log,
 		tokenTTL:     tokenTTL,
 	}
+}
+
+func (a *Auth) Login(ctx context.Context, email, password string, appId int) (string, error) {
+	const op = "auth.Login"
+
+	log := a.log.With(slog.String("op", op), slog.String("username", email))
+	log.Info("attempting to login user")
+
+	user, err := a.userProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", sl.Err(err))
+			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+		a.log.Error("failed to get user", sl.Err(err))
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		a.log.Info("invalid credentials", sl.Err(err))
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := a.appProvider.App(ctx, appId)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("user logged is successfully")
+
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed to generate token", sl.Err(err))
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, nil
+}
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email, password string) (int64, error) {
+	const op = "auth.RegisterNewUser"
+
+	log := a.log.With(slog.String("op", op), slog.String("email", email))
+	log.Info("registering new user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := a.userSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save suer", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	log.Info("user saved")
+
+	return id, nil
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "Auth.IsAdmin"
+
+	log := a.log.With(slog.String("op", op), slog.Int64("user_id", userID))
+	log.Info("checking if user is admin")
+
+	isAdmin, err := a.userProvider.IsAdmin(ctx, userID)
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+	log.Info("checked if user is admin", slog.Bool("is_admin", isAdmin))
+	return isAdmin, nil
 }
